566091acf9bf50259d2fb82453ffcef531feaa59
Kumar Satyam <66054160+krsatyam278@users.noreply.github.com>
2022-07-07 19:27:56 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import EmojiObjectsIcon from "@material-ui/icons/EmojiObjects";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <EmojiObjectsIcon />
+        Keep notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
